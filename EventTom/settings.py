5f4f92466d8c5fe54068b0846ffdf00d82e1d3ae
Max Tepper <FlashwarLP@gmail.com>
2024-11-29 10:18:41 +0100

    - update settings

--- EventTom/settings.py
+++ EventTom/settings.py
@@ -107,7 +107,7 @@ tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', '').encode(),
+        'NAME': str(tmpPostgres.path.replace('/', '')),
         'USER': tmpPostgres.username,
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
