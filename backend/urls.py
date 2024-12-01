35ba0af2376ff6b28cb5aadbb64901a9d4279bed
Skalix <skalix@protonmail.com>
2024-12-01 18:09:06 +0100

    [#1] Add login and register routes (wip)

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.urls import path
 
+from backend import views
+
 urlpatterns = [
-    
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import UserManager
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class LoginView(APIView):
+    authentication_classes = [JWTAuthentication]
+
+    @staticmethod
+    def post(request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if not username or not password:
+            return Response({'detail': 'username and password are required'}, status=401)
+        # user = UserManager.authenticate(username=username, password=password)
+
+class RegisterView(APIView):
+    @staticmethod
+    def post(request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if not username or not password:
+            return Response({'detail': 'username and password are required'}, status=401)
+        # user = UserManager.create(username=username, password=password)
