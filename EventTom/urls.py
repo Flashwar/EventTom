088cc32234d6faf61e3a94539e1cbf766e9f6fdd
Max Tepper <tepper@th-brandenburg.de>
2024-12-13 17:11:59 +0100

    - try this

--- EventTom/urls.py
+++ EventTom/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from EventTom import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('backend.urls')),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
